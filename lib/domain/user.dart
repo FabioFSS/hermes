dbd98eb1b8dfce43fd9f33c6417bcb2268fdb2a4
Fabiano Santos <Fabio_FSS@outlook.com>
2024-09-26 21:43:40 -0300

    feat: create user model

---
+++ lib/domain/user.dart
@@ -0,0 +1,13 @@
+class User {
+  String username;
+
+  User(this.username);
+
+  getUsername() {
+    return username;
+  }
+
+  setUsername(String username) {
+    this.username = username;
+  }
+}
